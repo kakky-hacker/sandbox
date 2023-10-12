b78dcf45c89e85b0ce0e7bfa885fa505c5363deb
kakky-hacker <kakinouet@gmail.com>
2023-10-13 01:25:02 +0900

    commit job.py

--- prometheus_sample/job.py
+++ prometheus_sample/job.py
@@ -1,6 +1,12 @@
+from random import randint
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
 registry = CollectorRegistry()
-g = Gauge('job_last_complete_unixtime', 'Last time a batch job completed', registry=registry)
-g.set_to_current_time()
-push_to_gateway('localhost:9091', job='cronJobA', registry=registry)
+
+g_time = Gauge('job_last_complete_unixtime', 'Last time a batch job completed', registry=registry)
+g_time.set_to_current_time()
+
+g_num = Gauge('job_random_number', 'random number', registry=registry)
+g_num.set(randint(1, 20))
+
+push_to_gateway('localhost:9091', job='my-job', registry=registry)
