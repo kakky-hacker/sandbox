7f4589f442d3057a982fffc336bf1a4363d2fb9a
TakumaKakinoue <kakinouet@gmail.com>
2023-10-13 12:21:32 +0900

    commit job.py

--- prometheus_sample/job.py
+++ prometheus_sample/job.py
@@ -1,13 +1,27 @@
+import time
 from random import randint
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
-registry = CollectorRegistry()
 
-g_time = Gauge('job_last_complete_unixtime', 'Last time a batch job completed', registry=registry)
-g_time.set_to_current_time()
+def main():
+    registry = CollectorRegistry()
+    g_time = Gauge('job_last_complete_unixtime', 'Last time a batch job completed', registry=registry)
+    g_num = Gauge('job_random_number', 'random number', ['id'], registry=registry)
 
-g_num = Gauge('job_random_number', 'random number', ['id'], registry=registry)
-g_num.labels(id='aaa').set(randint(1, 20))
-g_num.labels(id='bbb').set(randint(1, 20))
+    while True:
+        g_time.set_to_current_time()
+        g_num.labels(id='aaa').set(randint(1, 20))
+        g_num.labels(id='bbb').set(randint(1, 20))
 
-push_to_gateway('localhost:9091', job='my-job', registry=registry)
+        push_to_gateway('localhost:9091', job='my-job', registry=registry)
+
+        logging.info("push metrics!!")
+
+        time.sleep(3)
+
+if __name__ == "__main__":
+    main()
